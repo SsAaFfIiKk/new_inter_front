e1911fa4858a2e49ab0ed080e5e6af6ba2f9050d
Alex L <71562303+4in4in@users.noreply.github.com>
2022-01-20 15:02:33 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { Interview } from './pages/Interview'
 
 export default function App() {
     return (
-        <Router basename="/interview">
+        <Router basename="questionnaires/interview">
             <Switch>
                 <Route path="/login">
                     <CheckUser />
